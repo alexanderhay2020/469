af5bba2b0faa4ae88ea8da9fdc0402ebef5dc03e
alex hay <alexanderhay2020@u.northwestern.edu>
2019-09-29 23:18:21 -0500

    Fancied up graph

--- HW0A/hw0a.py
+++ HW0A/hw0a.py
@@ -21,46 +21,84 @@ initialt=1288971842.041
 
 def odo():
 
-    # 2D array to hold calculated displacement and theta values, another for x&Y values
-    displacement=np.zeros((len(odometry),2))
-    delta=np.zeros((len(odometry),2))
+    # 4D array to hold calculated displacement, theta, x, and Y values
+    delta=np.zeros((len(odometry),4))
+
+    delta[0,2]=initialx
+    delta[0,3]=initialy
 
     # calulates mometary displacement and theta
     for i in range(len(odometry)):
         if i==0:
-            displacement[i,0]=odometry[i,1]*(odometry[i,0]-initialt) # finds displacement
-            displacement[i,1]=odometry[i,2]*(odometry[i,0]-initialt) # finds delta theta
+            delta[i,0]=odometry[i,1]*(odometry[i,0]-initialt) # finds displacement (m)
+            delta[i,1]=odometry[i,2]*(odometry[i,0]-initialt) # finds delta theta (rad)
+
         else:
-            displacement[i,0]=odometry[i,1]*(odometry[i,0]-odometry[i-1,0]) # finds displacement
-            displacement[i,1]=odometry[i,2]*(odometry[i,0]-odometry[i-1,0]) # findsa delta theta
+            delta[i,0]=odometry[i,1]*(odometry[i,0]-odometry[i-1,0]) # finds displacement (m)
+            delta[i,1]=odometry[i,2]*(odometry[i,0]-odometry[i-1,0]) # finds delta theta (rad)
 
-    # translates displacement and theta into x & y coordinates
-    deltax=np.cos(displacement[:,1])*displacement[:,0]
-    deltay=np.sin(displacement[:,1])*displacement[:,0]
+    # translates displacement into x & y coordinates
 
-    #delta[:,0]=np.cos(displacement[:,1])*displacement[:,0]
-    #delta[:,1]=np.sin(displacement[:,1])*displacement[:,0]
-
-    delta[0,0]=initialx
-    delta[0,1]=initialy
+    for i in range(len(delta)):
+        if i==0:
+            delta[i,2]=np.cos(delta[i,1]-initialrad)*delta[i,0]
+            delta[i,3]=np.sin(delta[i,1]-initialrad)*delta[i,0]
+        else:
+            delta[i,2]=np.cos(delta[i,1]+delta[i-1,1])*delta[i,0]
+            delta[i,3]=np.sin(delta[i,1]+delta[i-1,1])*delta[i,0]
 
     for i in range(1,len(delta)):
-        delta[i,0]=delta[i-1,0]+deltax[i]
-        delta[i,1]=delta[i-1,1]+deltay[i]
-    
+        delta[i,2]=delta[i-1,2]+delta[i,2]
+        delta[i,3]=delta[i-1,3]+delta[i,3]
+
     return delta
+
+def plotbarriers():
+
+    # plots graph title and axes
+    p.title("Global Position")
+    p.xlabel("x position (m)")
+    p.ylabel("y position (m)")
+
+    # plots landmark positions
+    p.plot(landmark[:,1],landmark[:,2],'ro')
+
+    # plots barriers
+    p.plot([landmark[1,1],landmark[4,1]],[landmark[1,2],landmark[4,2]],'k-') # connects landmarks 7 and 10
+    p.plot([landmark[4,1],landmark[3,1]],[landmark[4,2],landmark[3,2]],'k-') # connects landmarks 10 and 9
+    p.plot([landmark[3,1],landmark[0,1]],[landmark[3,2],landmark[0,2]],'k-') # connects landmarks 9 and 6
+    p.plot([landmark[0,1],landmark[2,1]],[landmark[0,2],landmark[2,2]],'k-') # connects landmarks 6 and 8
+    p.plot([landmark[2,1],landmark[5,1]],[landmark[2,2],landmark[5,2]],'k-') # connects landmarks 8 and 11
+    p.plot([landmark[5,1],landmark[6,1]],[landmark[5,2],landmark[6,2]],'k-') # connects landmarks 11 and 12
+    p.plot([landmark[6,1],landmark[14,1]],[landmark[6,2],landmark[14,2]],'k-') # connects landmarks 12 and 20
+    p.plot([landmark[14,1],landmark[13,1]],[landmark[14,2],landmark[13,2]],'k-') # connects landmarks 20 and 19
+    p.plot([landmark[13,1],landmark[12,1]],[landmark[13,2],landmark[12,2]],'k-') # connects landmarks 19 and 18
+    p.plot([landmark[12,1],landmark[11,1]],[landmark[12,2],landmark[11,2]],'k-') # connects landmarks 18 and 17
+    p.plot([landmark[11,1],landmark[9,1]],[landmark[11,2],landmark[9,2]],'k-') # connects landmarks 17 and 15
+    p.plot([landmark[9,1],landmark[4,1]],[landmark[9,2],landmark[4,2]],'k-') # connects landmarks 15 and 10
+
+    p.plot([landmark[10,1],landmark[8,1]],[landmark[10,2],landmark[8,2]],'k-') # connects landmarks 16 and 14
+    p.plot([landmark[8,1],landmark[7,1]],[landmark[8,2],landmark[7,2]],'k-') # connects landmarks 14 and 13
+
+    # plots landmark annotations
+    for i in range(len(landmark)):
+        p.annotate(str(landmark[i,0])[:-2], # landmark # label
+        (landmark[i,1],landmark[i,2]), # landmark coordinates for label
+        textcoords="offset points", # how to position text
+        xytext=(7,10), # distance from text to points (x,y)
+        ha='center') # horizontal adjustment; left, right, or center
+
+    # plots position data from motion capture
+    p.plot(groundtruth[:,1],groundtruth[:,2],'b-',label='groundtruth')
+
+    # creates legend
+    p.legend(loc='best')
 
 def main():
 
-    # plots position data from motion capture
-    #p.plot(groundtruth[:,1],groundtruth[:,2])
-
-    # plots landmark positions
-    p.plot(landmark[:,1],landmark[:,2],'rx')
-
     delta=odo()
-
-    p.plot(delta[:,0],delta[:,1],'bo')
+    plotbarriers()
+    #p.plot(delta[:,2],delta[:,3],'bo')
 
     p.show()
 
