1475da8dbcaf5a5e9f3d3ed989c5bbf61faaf59b
alex hay <alexanderhay2020@u.northwestern.edu>
2019-10-18 00:46:49 -0500

    Cleared irrelevant code

--- HW1A/run.py
+++ HW1A/run.py
@@ -13,38 +13,33 @@ odometry = np.loadtxt('ds1_Odometry.dat') # time, forward v, angular v, measured
 def grid(): # displays environment
 
     # plots graph axes
+    plt.title('Environment')
     plt.xlabel("x position (m)")
     plt.ylabel("y position (m)")
-    plt.xlim(-2,5)
-    plt.ylim(-6,6)
+
     plt.autoscale = True
 
     # plots landmark positions
     plt.plot(landmark[:, 1], landmark[:, 2], 'ro')
 
     # plots landmark annotations
-    # for i in range(len(landmark)):
-    #     plt.annotate(str(landmark[i, 0])[:-2],  # landmark # label
-    #                # landmark coordinates for label
-    #                (landmark[i, 1], landmark[i, 2]),
-    #                textcoords="offset points",  # how to position text
-    #                xytext=(7, 10),  # distance from text to points (x,y)
-    #                ha='center')  # horizontal adjustment; left, right, or center
+    for i in range(len(landmark)):
+        plt.annotate(str(landmark[i, 0])[:-2],  # landmark # label
+                   # landmark coordinates for label
+                   (landmark[i, 1], landmark[i, 2]),
+                   textcoords="offset points",  # how to position text
+                   xytext=(7, 10),  # distance from text to points (x,y)
+                   ha='center')  # horizontal adjustment; left, right, or center
 
 
     xedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
     yedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 
-    # x = np.zeros(100)
-    # y = np.zeros(100)
-    # H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
     # H = H.T  # Let each row list bins with common y range.
 
     plt.hist2d(landmark[:, 1], landmark[:, 2], bins=(xedges, yedges), cmap=plt.cm.BuGn_r)
-    fig = plt.figure(figsize=(5, 5))
-    #ax = fig.add_subplot(title='imshow: square bins')
-    # plt.imshow(H, interpolation='nearest', origin='low',
-    #        extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+    plt.xlim(-2,5)
+    plt.ylim(-6,6)
     plt.grid()
     plt.show()
 
