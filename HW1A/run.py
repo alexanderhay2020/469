7bcd792595d88466f6639dbb6b9afac18cb34db5
alex hay <alexanderhay2020@u.northwestern.edu>
2019-10-18 15:33:55 -0500

    corrected histogram axis scales

--- HW1A/run.py
+++ HW1A/run.py
@@ -17,28 +17,6 @@ class grid():
         xedges = [-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7]
         yedges = [-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7]
 
-        grid = np.ones(([len(xedges),len(yedges)]))
-
-        startx = 0.5
-        starty = -1.5
-
-        # for i in range(len(landmark)):
-        #     for i_2 in range(len)landmark)):
-
-
-        #plt.imshow(grid.T,) # cmap, origin=lower, extent
-        # plots grid
-        fig = plt.figure()
-        #ax = fig.gca()
-        plt.title('Environment')
-        plt.xticks(xedges)
-        plt.yticks(yedges)
-        plt.xlabel("x position (m)")
-        plt.ylabel("y position (m)")
-        plt.autoscale = True
-
-
-
         # plots landmark positions
         #plt.plot(landmark[:, 1], landmark[:, 2], 'ro')
 
@@ -71,9 +49,30 @@ class grid():
 
 
         # H = H.T  # Let each row list bins with common y range.
+        fig2 = plt.figure()
+        # # plots barriers
+        plt.plot([landmark[1,1],landmark[4,1]],[landmark[1,2],landmark[4,2]],'r-') # connects landmarks 7 and 10
+        plt.plot([landmark[4,1],landmark[3,1]],[landmark[4,2],landmark[3,2]],'r-') # connects landmarks 10 and 9
+        plt.plot([landmark[3,1],landmark[0,1]],[landmark[3,2],landmark[0,2]],'r-') # connects landmarks 9 and 6
+        plt.plot([landmark[0,1],landmark[2,1]],[landmark[0,2],landmark[2,2]],'r-') # connects landmarks 6 and 8
+        plt.plot([landmark[2,1],landmark[5,1]],[landmark[2,2],landmark[5,2]],'r-') # connects landmarks 8 and 11
+        plt.plot([landmark[fig2 = plt.figure(5,1],landmark[6,1]],[landmark[5,2],landmark[6,2]],'r-') # connects landmarks 11 and 12
+        plt.plot([landmark[6,1],landmark[14,1]],[landmark[6,2],landmark[14,2]],'r-') # connects landmarks 12 and 20
+        plt.plot([landmark[14,1],landmark[13,1]],[landmark[14,2],landmark[13,2]],'r-') # connects landmarks 20 and 19
+        plt.plot([landmark[13,1],landmark[12,1]],[landmark[13,2],landmark[12,2]],'r-') # connects landmarks 19 and 18
+        plt.plot([landmark[12,1],landmark[11,1]],[landmark[12,2],landmark[11,2]],'r-') # connects landmarks 18 and 17
+        plt.plot([landmark[11,1],landmark[9,1]],[landmark[11,2],landmark[9,2]],'r-') # connects landmarks 17 and 15
+        plt.plot([landmark[9,1],landmark[4,1]],[landmark[9,2],landmark[4,2]],'r-') # connects landmarks 15 and 10
 
+        # plots island barriers
+        plt.plot([landmark[10,1],landmark[8,1]],[landmark[10,2],landmark[8,2]],'r-') # connects landmarks 16 and 14
+        plt.plot([landmark[8,1],landmark[7,1]],[landmark[8,2],landmark[7,2]],'r-') # connects landmarks 14 and 13
         plt.hist2d(landmark[:, 1], landmark[:, 2], bins=(xedges, yedges), cmap=plt.cm.BuGn_r)
-
+        plt.title('Histogram')
+        plt.xticks(xedges)
+        plt.yticks(yedges)
+        plt.xlabel("x position (m)")
+        plt.ylabel("y position (m)")
         # plt.xlim(-2,5)
         # plt.ylim(-6,6)
         plt.grid()
