8a8aba7792b9e2b2c2c601276c3d64ee9f51eecf
alex hay <alexanderhay2020@u.northwestern.edu>
2019-10-17 23:35:34 -0500

    beginning histogramgrid system

--- HW1A/run.py
+++ HW1A/run.py
@@ -10,7 +10,7 @@ landmark = np.loadtxt('ds1_Landmark_Groundtruth.dat')  # landmark data
 measurement = np.loadtxt('ds1_Measurement.dat') # measurement data from robot
 odometry = np.loadtxt('ds1_Odometry.dat') # time, forward v, angular v, measured from robot
 
-def plotbarriers(): # displays environment
+def grid(): # displays environment
 
     # plots graph axes
     p.xlabel("x position (m)")
@@ -22,24 +22,6 @@ def plotbarriers(): # displays environment
     # plots landmark positions
     p.plot(landmark[:, 1], landmark[:, 2], 'ro')
 
-    # plots barriers
-    p.plot([landmark[1, 1], landmark[4, 1]], [landmark[1, 2], landmark[4, 2]], 'k-')  # connects landmarks 7 and 10
-    p.plot([landmark[4, 1], landmark[3, 1]], [landmark[4, 2], landmark[3, 2]], 'k-')  # connects landmarks 10 and 9
-    p.plot([landmark[3, 1], landmark[0, 1]], [landmark[3, 2], landmark[0, 2]], 'k-')  # connects landmarks 9 and 6
-    p.plot([landmark[0, 1], landmark[2, 1]], [landmark[0, 2], landmark[2, 2]], 'k-')  # connects landmarks 6 and 8
-    p.plot([landmark[2, 1], landmark[5, 1]], [landmark[2, 2], landmark[5, 2]], 'k-')  # connects landmarks 8 and 11
-    p.plot([landmark[5, 1], landmark[6, 1]], [landmark[5, 2], landmark[6, 2]], 'k-')  # connects landmarks 11 and 12
-    p.plot([landmark[6, 1], landmark[14, 1]], [landmark[6, 2], landmark[14, 2]], 'k-')  # connects landmarks 12 and 20
-    p.plot([landmark[14, 1], landmark[13, 1]], [landmark[14, 2], landmark[13, 2]], 'k-')  # connects landmarks 20 and 19
-    p.plot([landmark[13, 1], landmark[12, 1]], [landmark[13, 2], landmark[12, 2]], 'k-')  # connects landmarks 19 and 18
-    p.plot([landmark[12, 1], landmark[11, 1]], [landmark[12, 2], landmark[11, 2]], 'k-')  # connects landmarks 18 and 17
-    p.plot([landmark[11, 1], landmark[9, 1]], [landmark[11, 2],  landmark[9, 2]], 'k-')  # connects landmarks 17 and 15
-    p.plot([landmark[9, 1], landmark[4, 1]], [landmark[9, 2], landmark[4, 2]], 'k-')  # connects landmarks 15 and 10
-
-    # plots island barriers
-    p.plot([landmark[10, 1], landmark[8, 1]], [landmark[10, 2], landmark[8, 2]], 'k-')  # connects landmarks 16 and 14
-    p.plot([landmark[8, 1], landmark[7, 1]], [landmark[8, 2], landmark[7, 2]], 'k-')  # connects landmarks 14 and 13
-
     # plots landmark annotations
     for i in range(len(landmark)):
         p.annotate(str(landmark[i, 0])[:-2],  # landmark # label
@@ -48,3 +30,37 @@ def plotbarriers(): # displays environment
                    textcoords="offset points",  # how to position text
                    xytext=(7, 10),  # distance from text to points (x,y)
                    ha='center')  # horizontal adjustment; left, right, or center
+
+   xedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+   yedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+
+   x = np.zeros(100)
+   y = np.zeros(100)
+   H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
+   H = H.T  # Let each row list bins with common y range.
+
+   fig = p.figure(figsize=(5, 5))
+   #ax = fig.add_subplot(title='imshow: square bins')
+   p.imshow(H, interpolation='nearest', origin='low',
+           extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+   p.grid()
+   p.show()
+
+class grid():
+
+    """
+    This is the grid class
+
+    """
+
+    def __init__(grid,coor,flag,start,goal):
+
+        grid = np.zeros((12,12))
+        for i in range(len(grid)):
+            grid[i] = np.arange(i,i+1,0.1)
+
+
+        # need to map index to grid space
+        grid.flag = flag # occupied, unoccupied
+        grid.coor.x = x
+        grid.coor.y = y
