52f4ff12565564ac9aff77ab7b4c1b4ce8e0e96b
alex hay <alexanderhay2020@u.northwestern.edu>
2019-10-18 00:41:28 -0500

    DONT TOUCH ANYTHING AND MARVEL AT IT

--- HW1A/run.py
+++ HW1A/run.py
@@ -1,7 +1,7 @@
 # Draws Grid
 
 import numpy as np
-import pylab as p
+import matplotlib.pyplot as plt
 
 # global variables
 barcodes = np.loadtxt('ds1_Barcodes.dat')
@@ -13,38 +13,40 @@ odometry = np.loadtxt('ds1_Odometry.dat') # time, forward v, angular v, measured
 def grid(): # displays environment
 
     # plots graph axes
-    p.xlabel("x position (m)")
-    p.ylabel("y position (m)")
-    p.xlim(-8, 10)
-    p.ylim(-7, 7)
-    p.autoscale = True
+    plt.xlabel("x position (m)")
+    plt.ylabel("y position (m)")
+    plt.xlim(-2,5)
+    plt.ylim(-6,6)
+    plt.autoscale = True
 
     # plots landmark positions
-    p.plot(landmark[:, 1], landmark[:, 2], 'ro')
+    plt.plot(landmark[:, 1], landmark[:, 2], 'ro')
 
     # plots landmark annotations
-    for i in range(len(landmark)):
-        p.annotate(str(landmark[i, 0])[:-2],  # landmark # label
-                   # landmark coordinates for label
-                   (landmark[i, 1], landmark[i, 2]),
-                   textcoords="offset points",  # how to position text
-                   xytext=(7, 10),  # distance from text to points (x,y)
-                   ha='center')  # horizontal adjustment; left, right, or center
+    # for i in range(len(landmark)):
+    #     plt.annotate(str(landmark[i, 0])[:-2],  # landmark # label
+    #                # landmark coordinates for label
+    #                (landmark[i, 1], landmark[i, 2]),
+    #                textcoords="offset points",  # how to position text
+    #                xytext=(7, 10),  # distance from text to points (x,y)
+    #                ha='center')  # horizontal adjustment; left, right, or center
 
-   xedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
-   yedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 
-   x = np.zeros(100)
-   y = np.zeros(100)
-   H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
-   H = H.T  # Let each row list bins with common y range.
+    xedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+    yedges = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 
-   fig = p.figure(figsize=(5, 5))
-   #ax = fig.add_subplot(title='imshow: square bins')
-   p.imshow(H, interpolation='nearest', origin='low',
-           extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
-   p.grid()
-   p.show()
+    # x = np.zeros(100)
+    # y = np.zeros(100)
+    # H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
+    # H = H.T  # Let each row list bins with common y range.
+
+    plt.hist2d(landmark[:, 1], landmark[:, 2], bins=(xedges, yedges), cmap=plt.cm.BuGn_r)
+    fig = plt.figure(figsize=(5, 5))
+    #ax = fig.add_subplot(title='imshow: square bins')
+    # plt.imshow(H, interpolation='nearest', origin='low',
+    #        extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+    plt.grid()
+    plt.show()
 
 grid()
 
