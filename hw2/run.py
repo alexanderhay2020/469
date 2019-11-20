36ff13d13df767d9a92c6db060d7c4de8e4e4cdf
alex hay <alexanderhay2020@u.northwestern.edu>
2019-11-19 23:19:55 -0600

    tidying

--- hw2/run.py
+++ hw2/run.py
@@ -5,10 +5,10 @@ HW2 - Machine Learning
 Learning aim: Motion model
 Learning algorithm: Neural Network
 Dataset: DS1
- + Training Data: (v, w) - odometry
+ + Training Data: (t, v, w) - odometry
                   (x, y, theta) - groundtruth
 
- + Test Data: (v, w) - odometry
+ + Test Data: (t, v, w) - odometry
 
 Part A
 1. build training set
@@ -19,15 +19,7 @@ Part A
 
 import numpy as np
 
-# global variables
-barcodes = np.loadtxt('ds1_Barcodes.dat')
-groundtruth = np.loadtxt('ds1_Groundtruth.dat') # position data from motion capture (may be taken as known information for filtering)
-landmark = np.loadtxt('ds1_Landmark_Groundtruth.dat')  # landmark data
-measurement = np.loadtxt('ds1_Measurement.dat') # measurement data from robot
-odometry = np.loadtxt('ds1_Odometry.dat') # time, forward v, angular v, measured from robot
-
 np.random.seed(1) # for troubleshooting, can reproduce
-
 
 def sigmoid(x):
     """
@@ -58,29 +50,19 @@ for i in range(len(input)):
     Motion Model
     """
 
-    time = input[i,0]       # time
-    v = input[i,4]          # velocity
-    theta = input[i,5]*time # dtheta = w*t
+    time = input[i,0]                # time
+    v = input[i,4]                   # linear velocity
+    w = input[i,5]                   # angular velocity
 
+    theta = w*time                   # dtheta = w*t
     delta_x = (v*np.cos(theta)*time) # dx = vt*cos(theta)
     delta_y = (v*np.sin(theta)*time) # dy = vt*sin(theta)
-    delta_theta = theta              # dtheta
 
     output[i,0] = delta_x
     output[i,1] = delta_y
-    output[i,2] = delta_theta
+    output[i,2] = theta
 
-weights = np.random.random((6,3)) # starting weight for each column (synapse)
-
-# input = input[:,:3]
-
-# print "Input: "
-# print input
-# print
-
-# print "Output: "
-# print output
-# print
+weights = np.random.random([6,3]) # starting weight for each column (synapse)
 
 print "Starting Weights: "
 print weights
@@ -103,7 +85,3 @@ for i in range(2000):
 print "Weights after training: "
 print weights
 print
-
-# print "Output: "
-# print output
-# print
